b8fc68160a1d161067b45be5ecadd44bc775f1ef
haizailache999 <90511158+haizailache999@users.noreply.github.com>
2024-04-19 12:52:30 -0500

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -12,20 +12,20 @@ import copy
 def get_args():
 	parser = argparse.ArgumentParser(description='Extended Deep Active Learning Toolkit')
 	#basic arguments
-	parser.add_argument('--ALstrategy', '-a', default='EntropySampling', type=str, help='name of active learning strategies')
-	parser.add_argument('--quota', '-q', default=1000, type=int, help='quota of active learning')
-	parser.add_argument('--batch', '-b', default=128, type=int, help='batch size in one active learning iteration')
+	parser.add_argument('--ALstrategy', '-a', default='BALDDropout', type=str, help='name of active learning strategies')
+	parser.add_argument('--quota', '-q', default=int(50000*0.30), type=int, help='quota of active learning')
+	parser.add_argument('--batch', '-b', default=int(50000*0.02), type=int, help='batch size in one active learning iteration')
 	parser.add_argument('--dataset_name', '-d', default='CIFAR10', type=str, help='dataset name')
-	parser.add_argument('--iteration', '-t', default=3, type=int, help='time of repeat the experiment')
+	parser.add_argument('--iteration', '-t', default=1, type=int, help='time of repeat the experiment')
 	parser.add_argument('--data_path', type=str, default='./../data', help='Path to where the data is')
 	parser.add_argument('--out_path', type=str, default='./../results', help='Path to where the output log will be')
 	parser.add_argument('--log_name', type=str, default='test.log', help='middle outputs')
 	#parser.add_argument('--help', '-h', default=False, action='store_true', help='verbose')
 	parser.add_argument('--cuda', action='store_true', help='If training is to be done on a GPU')
 	#parser.add_argument('--model', '-m', default='ResNet18', type=str, help='model name')
-	parser.add_argument('--initseed', '-s', default = 1000, type = int, help = 'Initial pool of labeled data')
-	parser.add_argument('--gpu', '-g', default = 0, type = str, help = 'which gpu')
-	parser.add_argument('--seed', default=4666, type=int, help='random seed')
+	parser.add_argument('--initseed', '-s', default = int(50000*0.025)*4, type = int, help = 'Initial pool of labeled data')
+	parser.add_argument('--gpu', '-g', default = 1, type = str, help = 'which gpu')
+	parser.add_argument('--seed', default=3407, type=int, help='random seed')
 	
 	# lpl
 	parser.add_argument('--lpl_epoches', type=int, default=20, help='lpl epoch num after detach')
