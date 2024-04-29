aa4ee28c2b341b9f15d47a58beda682790917a1c
haizailache999 <90511158+haizailache999@users.noreply.github.com>
2024-04-29 17:45:54 -0500

    Update get_idx.py

--- get_idx.py
+++ get_idx.py
@@ -7,8 +7,10 @@
 
 from query_strategy import LeastConfidence, MarginSampling, EntropySampling,KMeansSampling,KCenterGreedyPCA, BALDDropout,VarRatio, MeanSTD, BadgeSampling
 import numpy as np
+import math
 
 def get_idxs(dataset, net, args_input, args_task,NUM_QUERY,unlabeled_idxs,loader):
+    #print("dataset",len(dataset))
     net1=net
     #st1=RandomSampling(dataset, net, args_input, args_task)
     st2=LeastConfidence(dataset=dataset, net=net, args_input=args_input, args_task=args_task,loader=loader)
@@ -24,17 +26,29 @@ def get_idxs(dataset, net, args_input, args_task,NUM_QUERY,unlabeled_idxs,loader
     st12=BALDDropout(dataset, net, args_input, args_task,loader)
     st13=VarRatio(dataset, net, args_input, args_task,loader)
     st14=MeanSTD(dataset, net, args_input, args_task,loader)
-    st15=BadgeSampling(dataset, net, args_input, args_task,loader)
+    #st15=BadgeSampling(dataset, net, args_input, args_task,loader)
     #st16=AdversarialBIM(dataset, net, args_input, args_task)
     #st17=AdversarialDeepFool(dataset, net, args_input, args_task)
     #st_list=[st1,st2,st3,st4,st5,st6,st7,st8,st10,st11,st12,st13,st14,st15]
-    st_list=[st2,st3,st4,st8,st12,st13,st14,st15]
-    result_list=np.zeros((128, len(st_list)))
+    #st_list=[st2,st3,st4,st8,st12,st13,st14]
+    st_list=[st2,st3]
+    #print("loader length",len(loader.dataset))
+    result_list=np.zeros((len(loader.dataset), len(st_list)))
+    #print(NUM_QUERY)
     for i,strategy in enumerate(st_list):
-        q_idx=strategy.query(NUM_QUERY)
+        if len(loader.dataset)>NUM_QUERY:
+            #print("yes")
+            q_idx=strategy.query(1000)
+        else:
+            if math.ceil(len(loader.dataset)*0.02)==1:
+                num_q=2
+            else:
+                num_q=math.ceil(len(loader.dataset)*0.02)
+            q_idx=strategy.query(num_q)
         for t in q_idx:
             result_list[t][i]=1
         #for t_pos,t in enumerate(unlabeled_idxs):
             #if t in q_idx:
                 #result_list[t_pos][i]=1
+    #print(result_list)
     return result_list
