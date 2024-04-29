8c32d54910ff79bb4c25a7c8f6a0bdce924cdee3
haizailache999 <90511158+haizailache999@users.noreply.github.com>
2024-04-29 17:46:29 -0500

    Update AL_model.py

--- AL_model.py
+++ AL_model.py
@@ -37,35 +37,55 @@ class Architect(object):
         self.model = model
         self.model.train()
         #if '_1' in name
-        self.optimizer = torch.optim.Adam([{'params':[ param for name, param in model.named_parameters()]}],
+        self.optimizer1 = torch.optim.Adam([{'params':[ param for name, param in model.named_parameters() if '_1' in name]}],
+            lr=0.0005, betas=(0.5, 0.999), weight_decay=0.1)
+        self.optimizer2 = torch.optim.Adam([{'params':[ param for name, param in model.named_parameters() if '_1' not in name]}],
             lr=0.0005, betas=(0.5, 0.999), weight_decay=0.1)
         
-    def step(self, input_valid, target_valid,device,score,length):
-        self.optimizer.zero_grad()
-        #for name, parms in self.model.named_parameters():	
-            #print(name)
-        loss,loss1,loss2=self._backward_step(input_valid.to(device), target_valid.to(device),score,device,length)
-        self.optimizer.step()
-        '''for name, parms in self.model.named_parameters():	
-            #print('after-->name:', name)
-            #print('after-->para:', parms)
-            #print('after-->grad_requirs:',parms.requires_grad)
-            if parms.grad is not None:
-                if "_1" in name:
-                    print('after-->grad_value:',name)
-                    print(parms.grad)
-            #print("after===")
-            #break'''
+    def step(self, input_valid, target_valid,device,score,length,optim):
+        self.optimizer1.zero_grad()
+        self.optimizer2.zero_grad()
+        if optim==1:
+            #for name, parms in self.model.named_parameters():	
+                #print(name)
+            loss,loss1,loss2=self._backward_step(input_valid.to(device), target_valid.to(device),score,device,length)
+            self.optimizer1.step()
+            '''for name, parms in self.model.named_parameters():	
+                #print('after-->name:', name)
+                #print('after-->para:', parms)
+                #print('after-->grad_requirs:',parms.requires_grad)
+                if parms.grad is not None:
+                    if "_1" in name:
+                        print('after-->grad_value:',name)
+                        print(parms.grad)
+                #print("after===")
+                #break'''
+        else:
+            #for name, parms in self.model.named_parameters():	
+                #print(name)
+            loss,loss1,loss2=self._backward_step1(input_valid.to(device), target_valid.to(device),score,device,length)
+            self.optimizer2.step()
+            '''for name, parms in self.model.named_parameters():	
+                #print('after-->name:', name)
+                #print('after-->para:', parms)
+                #print('after-->grad_requirs:',parms.requires_grad)
+                if parms.grad is not None:
+                    if "_1" in name:
+                        print('after-->grad_value:',name)
+                        print(parms.grad)
+                #print("after===")
+                #break'''
         return loss,loss1,loss2
 
     def _backward_step(self, input_valid, target_valid,score,device,length):
         loss = F.cross_entropy(input_valid, target_valid,reduction='none')
+        loss=loss.detach()
         loss_test=F.cross_entropy(input_valid, target_valid,reduction='mean')
-        file = open("./loss_check9.txt", 'a')
+        #file = open("./loss_check9.txt", 'a')
         #file.write("loss:")
-        file.write(str(loss_test.item()))
-        file.write("  ")
-        n=torch.sum(score < 0.5).item()
+        #file.write(str(loss_test.item()))
+        #file.write("  ")
+        n=torch.sum(score > 0.5).item()
         #print("this is n",n,length*0.02)
         if n>length*0.02:
             add_loss=(1/(1+math.exp(0.5*(length*0.02-n)))-0.5)*2
@@ -80,7 +100,42 @@ class Architect(object):
         loss1=(loss*score).mean()
         loss2=add_loss
         #loss2=0
-        loss=loss1
+        loss=-loss1-loss2
+        #loss=score.mean()
+        #print("loss",loss)
+        #file = open("./check_loss.txt", 'a')
+        #file.write("loss:")
+        #loss=Variable(loss, requires_grad=True)
+        #print(loss.requires_grad)
+        #print(loss)
+        #with torch.autograd.detect_anomaly():
+        loss.backward()
+        return loss,loss1,loss2
+
+    def _backward_step1(self, input_valid, target_valid,score,device,length):
+        loss = F.cross_entropy(input_valid, target_valid,reduction='none')
+        loss_test=F.cross_entropy(input_valid, target_valid,reduction='mean')
+        #file = open("./loss_check10.txt", 'a')
+        #file.write("loss:")
+        #file.write(str(loss_test.item()))
+        #file.write("  ")
+        n=torch.sum(score > 0.5).item()
+        #print("this is n",n,length*0.02)
+        if n>length*0.02:
+            add_loss=(1/(1+math.exp(0.5*(length*0.02-n)))-0.5)*2
+        else:
+            add_loss=(1/(1+math.exp(0.5*(n-length*0.02)))-0.5)*2
+        #score=torch.from_numpy(score).requires_grad_(True).to(device)
+        #print("add",add_loss)
+        #print(loss.device,score.device)
+        #print(score.requires_grad)
+        #print(score)
+        #print(loss*score)
+        score=score.detach()
+        loss1=(loss*score).mean()
+        loss2=add_loss
+        #loss2=0
+        loss=loss1+loss2
         #loss=score.mean()
         #print("loss",loss)
         #file = open("./check_loss.txt", 'a')
